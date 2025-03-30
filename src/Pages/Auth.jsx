6951a4971a230b3e2bd3721d2842c4c78a724f65
David Hbt <davidhabte7@gmail.com>
2025-03-31 01:42:52 +0300

    testing auth

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Google_icon from '../assets/Google-icon.png'
 import User_icon from '../assets/WebP/User.webp'
 import './Auth.css'
-import { Auth_cfg } from '../Config/Firebase'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
 
 function Auth() {
   const [user, setUser] = useState()
-  const handleGoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(Auth_cfg, provider); // Use Auth_cfg directly
-      const user = result.user;
-      console.log("User signed in:", user);
-    } catch (error) {
-      console.error("Error during sign-in:", error);
+
+    useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const userInfo = window.Telegram.WebApp.initDataUnsafe.user;
+      setUser(userInfo);
     }
-  };
-
-  useEffect(() => {
-    setUser(Auth_cfg?.currentUser?.displayName)
-  }, [])
-
+  }, []);
   return (
     <div className='page'>
       <div className="AuthContainer">
@@ -32,10 +24,9 @@ function Auth() {
             
           </h1>
           <p className='AuthHP'>Who are you?</p>
-          <p>{user ? <>wsg</> : <>hell nah</>}</p>
         </div>
         <div className="AuthContent">
-          <div className="AuthCLogin" onClick={handleGoogleSignIn}>
+          <div className="AuthCLogin">
             <img src={Google_icon} alt="" className="AuthCImg" />
             <h1 className="AuthCH1">Sign in with  <span style={{color: 'var(--color1)'}}>Google</span></h1>
           </div>
